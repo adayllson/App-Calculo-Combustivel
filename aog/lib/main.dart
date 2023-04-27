97e4f599a5eae45f71ecf83aee447db1727a9680
Adailson Almeida <adayllson.almeida@gmail.com>
2023-04-27 15:49:16 -0300

    buttons

--- aog/lib/main.dart
+++ aog/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unnecessary_new, prefer_final_fields, must_be_immutable, non_constant_identifier_names
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unnecessary_new, prefer_final_fields, must_be_immutable, non_constant_identifier_names, prefer_const_literals_to_create_immutables
 
 import 'package:aog/widgets/input.widget.dart';
 import 'package:aog/widgets/logo.widget.dart';
@@ -34,41 +34,79 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Input(
-            ctrl: _gasCtrl, 
-            label: "Gasolina",
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
             ),
-            Input(
-            ctrl: _gasCtrl, 
-            label: "Álcool",
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Compensa utilizar álcool",
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 40,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+            margin: EdgeInsets.all(60),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(60),
             ),
-            Container(
-              margin: EdgeInsets.all(30),
-              height: 60,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(60,),
-              ),
-              child: FlatButton(
-              child: Text('CALCULAR'),
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 25,
-                fontFamily: "Big Shoulders Display",
+            child: TextButton(
+              child: Text(
+                "Calcular Novamente",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 30,
+                  fontFamily: "Big Shoulders Display",
+                ),
               ),
               onPressed: () {},
-            ),),
-            
+            ),
+          ),
+              ]),
+          ),
+          Input(
+            ctrl: _gasCtrl,
+            label: "Gasolina",
+          ),
+          Input(
+            ctrl: _gasCtrl,
+            label: "Álcool",
+          ),
+          Container(
+            margin: EdgeInsets.all(60),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(60),
+            ),
+            child: TextButton(
+              child: Text(
+                "Calcular",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 30,
+                  fontFamily: "Big Shoulders Display",
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
   }
-  
-  FlatButton({required Text child, required Null Function() onPressed, required TextStyle style}) {
-    
-  }
 }
-
-
-
-
